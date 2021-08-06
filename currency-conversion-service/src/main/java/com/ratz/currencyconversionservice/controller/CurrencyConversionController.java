3d5b28d0a81ee416d56fbac8d16a6aa7c40583a5
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-08-06 11:44:10 +0000

    added feign, did service invocation with feign.

--- currency-conversion-service/src/main/java/com/ratz/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/ratz/currencyconversionservice/controller/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.ratz.currencyconversionservice.controller;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.ratz.currencyconversionservice.entities.CurrencyConversion;
+import com.ratz.currencyconversionservice.proxy.CurrencyExchangeProxy;
 
 @RestController
 public class CurrencyConversionController {
+	
+	
+	@Autowired
+	private CurrencyExchangeProxy currencyEchangeProxy;
 
 	@GetMapping(value = "/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
@@ -33,7 +39,22 @@ public class CurrencyConversionController {
 				quantity,
 				currencyConversion.getConversionMultiple(),
 				quantity.multiply(currencyConversion.getConversionMultiple()),
-				currencyConversion.getEnvironment());
+				currencyConversion.getEnvironment()+ " " + "From rest template");
+	}
+	
+	@GetMapping(value = "/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		CurrencyConversion currencyConversion =  currencyEchangeProxy.retrieveExchangeValue(from, to);
+		
+		return new CurrencyConversion(currencyConversion.getId(),
+				from,
+				to,
+				quantity,
+				currencyConversion.getConversionMultiple(),
+				quantity.multiply(currencyConversion.getConversionMultiple()),
+				currencyConversion.getEnvironment()+ " " + "From feing");
 	}
 
 }
