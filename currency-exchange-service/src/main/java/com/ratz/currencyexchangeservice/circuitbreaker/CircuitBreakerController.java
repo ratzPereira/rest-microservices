f1ba64fb52f84d9f2384eea35d65aecabd20e5ee
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-08-08 12:50:54 +0000

    tried @RateLimit and @BulkHead features

--- currency-exchange-service/src/main/java/com/ratz/currencyexchangeservice/circuitbreaker/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/ratz/currencyexchangeservice/circuitbreaker/CircuitBreakerController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -17,14 +20,19 @@ public class CircuitBreakerController {
 	
 	
 	@GetMapping(value = "/sample-api")
-	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	//@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	//@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+	//@RateLimiter(name = "default") //10s -> only 10000 calls to sample api
+	@Bulkhead(name = "default")
 	public String sampleApi() {
 		
 		logger.info("Sample API call");
+//		
+//		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
 		
-		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+//		return forEntity.getBody();
 		
-		return forEntity.getBody();
+		return "Hi";
 	}
 	
 	public String hardcodedResponse(Exception ex) {
