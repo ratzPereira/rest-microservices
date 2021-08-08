0f4279fc992c9121a87232d6e15ada98bf29e873
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-08-08 12:20:03 +0000

    played with resilience4j, added dummy fallback method, changed wait time on @Retry and added exponential backoff

---
+++ currency-exchange-service/src/main/java/com/ratz/currencyexchangeservice/circuitbreaker/CircuitBreakerController.java
@@ -0,0 +1,34 @@
+package com.ratz.currencyexchangeservice.circuitbreaker;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
+
+@RestController
+public class CircuitBreakerController {
+
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+	
+	
+	@GetMapping(value = "/sample-api")
+	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	public String sampleApi() {
+		
+		logger.info("Sample API call");
+		
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+		
+		return forEntity.getBody();
+	}
+	
+	public String hardcodedResponse(Exception ex) {
+		return "fallback method";
+	}
+	
+}
