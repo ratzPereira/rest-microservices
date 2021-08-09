0ae79869461a64bca24a5b03f51f2d0ddece0110
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-08-09 14:35:04 +0000

    added api-gateway to docker compose. now we run all microservices with docker images and docker compose

--- currency-conversion-service/src/main/java/com/ratz/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/ratz/currencyconversionservice/controller/CurrencyConversionController.java
@@ -28,7 +28,7 @@ public class CurrencyConversionController {
 		uriVariables.put("from", from);
 		uriVariables.put("to", to);
 		
-		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000//currency-exchange/from/{from}/to/{to}",
+		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://currency-exchange:8000//currency-exchange/from/{from}/to/{to}",
 				CurrencyConversion.class, uriVariables);
 
 		CurrencyConversion currencyConversion =  responseEntity.getBody();
